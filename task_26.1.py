e93747d17b2ebb34431cafb02bd9fe1db8982a4b
mishutka200101 <79220116+mishutka200101@users.noreply.github.com>
2021-05-14 23:10:41 +0300

    Create task_26.1.py

---
+++ task_26.1.py
@@ -0,0 +1,19 @@
+from PIL import Image, ImageDraw
+
+
+def gradient(choose):
+    new_image = Image.new("RGB", (512, 200), (0, 0, 0))
+    draw = ImageDraw.Draw(new_image)
+    r = 0
+    g = 0
+    b = 0
+    for i in range(new_image.size[0]):
+        draw.line((i, 0, i, 512), fill=(r, g, b), width=2)
+        if i % 4 == 0:
+            if choose == "R":
+                r += 2
+            elif choose == "G":
+                g += 2
+            else:
+                b += 2
+    new_image.save("res.png", "PNG")
